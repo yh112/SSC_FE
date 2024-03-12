cc1e778bf6b2e59ff3432df335842dd45f91faec
yh112 <msh970011@naver.com>
2024-03-12 21:34:39 +0900

    Add Connect.js

--- src/App.js
+++ src/App.js
@@ -1,128 +1,19 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
-import useEditorScroll from "./useEditorScroll";
-import "./App.css";
-import hljs from "highlight.js";
-import "highlight.js/styles/github.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Connect from "./Connect";
+import CodeEditor from "./CodeEditor";
+import './index.css';
 
-const CodeEditor = () => {
-  const { lineRef, textRef, preRef, handleScrollChange } = useEditorScroll();
-  const [lineCount, setLineCount] = useState(0);
-
-  const [highlightedHTML, setHighlightedCode] = useState("");
-  const [code, setCode] = useState("");
-  const [language, setLanguage] = useState("java");
-  const [tapCount, setTapCount] = useState(0);
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(0);
-
-  useEffect(() => {
-    textRef.current?.focus();
-  }, []);
-
-  useEffect(() => {
-    setLineCount(code.split("\n").length);
-  }, [code]);
-
-  useEffect(() => {
-    setHighlightedCode(
-      hljs.highlight(code, { language }).value.replace(/" "/g, "&nbsp; ")
-    );
-  }, [code, language]);
-
-  const changeCode = (e) => {
-    setCode(e.target.value);
-  };
-
-  const createMarkUpCode = (code) => ({
-    __html: code,
-  });
-
-  const changeLanguage = (e) => {
-    setLanguage(e.target.value);
-  };
-
-  const handleResizeHeight = useCallback(() => {
-    textRef.current.style.height = textRef.current.scrollHeight + "px";
-  }, []);
-
-  const handleKeyup = (e) => {
-    if(e.key === "{") {
-      setStart(e.target.selectionStart);
-      setEnd(e.target.selectionEnd);
-      setCode(
-        code.substring(0, start) + "}" + code.substring(end, code.length)
-      );
-      console.log(code);
-      e.target.selectionStart = e.target.selectionEnd = start;
-    }
-  }
-
-  const handleKeydown = (e) => { {
-    {
-      {
-        {
-          {
-            {
-              {                            
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-    if(e.key === "Tab") {
-      e.preventDefault();
-      setStart(e.target.selectionStart);
-      setEnd(e.target.selectionEnd);
-      console.log(start, end);
-      setCode(
-        code.substring(0, start) + "\t" + code.substring(end, code.length)
-      );
-      e.target.selectionStart = e.target.selectionEnd = start + 1;
-    }
-  }
-
+function App() {
   return (
     <div>
-      <select onChange={(e) => changeLanguage(e)}>
-        <option value="java">Java</option>
-        <option value="javascript">Javascript</option>
-        <option value="python">Python</option>
-        <option value="c">C</option>
-        <option value="cpp">C++</option>
-      </select>
-
-      <div className="code-editor">
-        <div className="code__lines" ref={lineRef}>
-          {Array.from(Array(lineCount + 1).keys())
-            .slice(1)
-            .join("\n")}
-        </div>
-
-        <div>
-          <textarea
-            ref={textRef}
-            onScroll={handleScrollChange}
-            value={code}
-            onChange={changeCode}
-            className="code-editor__textarea"
-            rows={1}
-            onInput={handleResizeHeight}
-            onKeyUp={handleKeyup}
-            onKeyDown={handleKeydown}
-            autoComplete="false"
-            spellCheck="false"
-          />
-          <pre className="code-editor__present">
-            <code
-              dangerouslySetInnerHTML={createMarkUpCode(highlightedHTML)}
-            ></code>
-          </pre>
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/connect" element={<Connect/>} />
+          <Route path="/editor/:editorId" element={<CodeEditor/>}/>
+        </Routes>
+      </Router>
     </div>
   );
-};
+}
 
-export default CodeEditor;
+export default App;
