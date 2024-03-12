eec0670182bdbde9c19cbb57b32df152e10632b3
yh112 <msh970011@naver.com>
2024-03-12 14:16:34 +0900

    Backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState, useRef } from "react";
+import useEditorScroll from "./useEditorScroll";
+import "./App.css";
+import hljs from "highlight.js";
+import "highlight.js/styles/github.css";
 
-function App() {
+const CodeEditor = () => {
+  const { lineRef, textRef, preRef, handleScrollChange } = useEditorScroll();
+  const [lineCount, setLineCount] = useState(0);
+
+  const [highlightedHTML, setHighlightedCode] = useState("");
+  const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("java");
+  const [tapCount, setTapCount] = useState(0);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(0);
+
+  useEffect(() => {
+    textRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    setLineCount(code.split("\n").length);
+  }, [code]);
+
+  useEffect(() => {
+    setHighlightedCode(
+      hljs.highlight(code, { language }).value.replace(/" "/g, "&nbsp; ")
+    );
+  }, [code, language]);
+
+  const changeCode = (e) => {
+    setCode(e.target.value);
+  };
+
+  const createMarkUpCode = (code) => ({
+    __html: code,
+  });
+
+  const changeLanguage = (e) => {
+    setLanguage(e.target.value);
+  };
+
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  }, []);
+
+  const handleKeyup = (e) => {
+    if(e.key === "{") {
+      setStart(e.target.selectionStart);
+      setEnd(e.target.selectionEnd);
+      setCode(
+        code.substring(0, start) + "}" + code.substring(end, code.length)
+      );
+      console.log(code);
+      e.target.selectionStart = e.target.selectionEnd = start;
+    }
+  }
+
+  const handleKeydown = (e) => { {
+    {
+      {
+        {
+          {
+            {
+              {                            
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+    if(e.key === "Tab") {
+      e.preventDefault();
+      setStart(e.target.selectionStart);
+      setEnd(e.target.selectionEnd);
+      console.log(start, end);
+      setCode(
+        code.substring(0, start) + "\t" + code.substring(end, code.length)
+      );
+      e.target.selectionStart = e.target.selectionEnd = start + 1;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select onChange={(e) => changeLanguage(e)}>
+        <option value="java">Java</option>
+        <option value="javascript">Javascript</option>
+        <option value="python">Python</option>
+        <option value="c">C</option>
+        <option value="cpp">C++</option>
+      </select>
+
+      <div className="code-editor">
+        <div className="code__lines" ref={lineRef}>
+          {Array.from(Array(lineCount + 1).keys())
+            .slice(1)
+            .join("\n")}
+        </div>
+
+        <div>
+          <textarea
+            ref={textRef}
+            onScroll={handleScrollChange}
+            value={code}
+            onChange={changeCode}
+            className="code-editor__textarea"
+            rows={1}
+            onInput={handleResizeHeight}
+            onKeyUp={handleKeyup}
+            onKeyDown={handleKeydown}
+            autoComplete="false"
+            spellCheck="false"
+          />
+          <pre className="code-editor__present">
+            <code
+              dangerouslySetInnerHTML={createMarkUpCode(highlightedHTML)}
+            ></code>
+          </pre>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CodeEditor;
