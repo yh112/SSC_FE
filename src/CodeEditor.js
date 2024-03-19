7f93fa8fab71322cf2386e75a7c7ee9e048d5c5b
yh112 <msh970011@naver.com>
2024-03-19 15:30:43 +0900

    Add auto indentation in CodeEditor

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -1,23 +1,25 @@
 import React, { useCallback, useEffect, useState, useRef } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useEditorScroll from "./useEditorScroll";
 import SockJS from "sockjs-client";
-import axios from "axios";
+// import axios from "axios";
 import * as StompJs from "@stomp/stompjs";
 import "./App.css";
 import hljs from "highlight.js";
 import "highlight.js/styles/github.css";
 
 const CodeEditor = () => {
-  const { lineRef, textRef, preRef, handleScrollChange } = useEditorScroll();
+  const { lineRef, textRef, handleScrollChange } = useEditorScroll();
   const [lineCount, setLineCount] = useState(0);
-
   const [highlightedHTML, setHighlightedCode] = useState("");
   const [code, setCode] = useState("");
   const [copyCode, setCopyCode] = useState("");
   const [language, setLanguage] = useState("java");
-  const [tapCount, setTapCount] = useState(0);
-  const [start, setStart] = useState(0);
+  const [cursor, setCursor] = useState(0);
+  let tabCount = 0;
+  let leftBracketPosition = [];
+  let rightBracketPosition = [];
+  let enterCount = 0;
 
   const client = useRef();
   let { editorId } = useParams();
@@ -46,6 +48,7 @@ const CodeEditor = () => {
     client.current.activate();
   };
 
+  //TODO: 실시간으로 데이터 주는 코드 추가
   const publish = () => {
     if (!client.current.connected) return;
 
@@ -54,18 +57,26 @@ const CodeEditor = () => {
       body: JSON.stringify({
         roomId: editorId,
         code: code,
+        start: cursor,
         line: lineCount,
       }),
     });
   };
 
+  //TODO: 실시간으로 데이터 받는 코드 추가
   const subscribe = () => {
     console.log("subscribe: " + client.current.connected);
     client.current.subscribe(`/subscribe/notice/${editorId}`, (body) => {
       const json_body = JSON.parse(body.body);
       console.log(json_body);
       setCopyCode(json_body.code);
+      //   addCode(json_body.code, json_body.start);
     });
+  };
+
+  const addCode = (text, start) => {
+    const value = code.substring(0, start) + text + code.substring(start);
+    setCode(value);
   };
 
   const disconnect = () => {
@@ -81,6 +92,25 @@ const CodeEditor = () => {
     publish();
   }, [code]);
 
+  const findBracket = () => {
+    console.log(code);
+    leftBracketPosition = [];
+    rightBracketPosition = [];
+    const rightExp = /\}/g;
+    const leftExp = /\{/g;
+    let leftPos, rightPos;
+    while (
+      (leftPos = leftExp.exec(code)) !== null &&
+      (rightPos = rightExp.exec(code)) !== null
+    ) {
+      console.log(leftPos.index);
+      console.log(rightPos.index);
+      leftBracketPosition.push(leftPos.index);
+      rightBracketPosition.push(rightPos.index);
+    }
+    console.log(leftBracketPosition, rightBracketPosition);
+  };
+
   useEffect(() => {
     setHighlightedCode(
       hljs.highlight(code, { language }).value.replace(/" "/g, "&nbsp; ")
@@ -88,8 +118,8 @@ const CodeEditor = () => {
   }, [code, language]);
 
   useEffect(() => {
-    textRef.current.setSelectionRange(start, start);
-  }, [start]);
+    textRef.current.setSelectionRange(cursor, cursor);
+  }, [cursor]);
 
   const changeCode = (e) => {
     setCode(e.target.value);
@@ -103,28 +133,25 @@ const CodeEditor = () => {
     setLanguage(e.target.value);
   };
 
-  const handleResizeHeight = useCallback(() => {
-    textRef.current.style.height = textRef.current.scrollHeight + "px";
-  }, []);
+  //   const handleResizeHeight = useCallback(() => {
+  //     textRef.current.style.height = textRef.current.scrollHeight + "px";
+  //   }, []);
 
-  //   const handleKeyup = (e) => {
-  //     if(e.key === "{") {
-  //       setCode(
-  //         code.substring(0, start) + "}" + code.substring(end, code.length)
-  //       );
-  //       console.log(code);
-  //       e.target.selectionStart = e.target.selectionEnd = start;
-  //     }
-  //   }
+  const handleResizeHeight = () => {
+    textRef.current.style.height = "auto";
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  };
 
   const handleKeydown = (e) => {
+    // setCursor(e.target.selectionStart);
+    console.log(e.target.selectionStart);
     if (e.key === "Tab") {
       e.preventDefault();
       const start = e.target.selectionStart;
       const end = e.target.selectionEnd;
       const value = code.substring(0, start) + "\t" + code.substring(end);
       textRef.value = value;
-      setStart(start+1);
+      setCursor(start + 1);
       setCode(value);
     } else if (e.key === "{") {
       e.preventDefault();
@@ -132,21 +159,75 @@ const CodeEditor = () => {
       const end = e.target.selectionEnd;
       const value = code.substring(0, start) + "{}" + code.substring(end);
       textRef.value = value;
-      setStart(start + 1);
+      setCursor(start + 1);
       setCode(value);
+    } else if (e.key === "(") {
+      e.preventDefault();
+      const start = e.target.selectionStart;
+      const end = e.target.selectionEnd;
+      const value = code.substring(0, start) + "()" + code.substring(end);
+      textRef.value = value;
+      setCursor(start + 1);
+      setCode(value);
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+      findBracket();
+      const start = e.target.selectionStart;
+      if (leftBracketPosition.length > 0) {
+        for (let i = 0; i < leftBracketPosition.length; i++) {
+          if (start > leftBracketPosition[i]) {
+            tabCount++;
+          }
+          if (start > rightBracketPosition[i]) {
+            tabCount--;
+          }
+          if (start === Number(rightBracketPosition[i])) {
+            enterCount++;
+          }
+        }
+        console.log(
+          "tab: " +
+            tabCount +
+            " enter: " +
+            enterCount +
+            " left: " +
+            leftBracketPosition +
+            " right: " +
+            rightBracketPosition +
+            " pos: " +
+            start
+        );
+      }
+
+      if (tabCount === 0) {
+        //그냥 엔터
+        const value = code.substring(0, start) + "\n" + code.substring(start);
+        textRef.value = value;
+        setCode(value);
+      } else if (tabCount > 0 && enterCount > 0) {
+        //바로 뒤에 닫힌 대괄호가 있을 때
+        const value =
+          code.substring(0, start) +
+          "\n" +
+          "\t".repeat(tabCount) +
+          "\n" +
+          "\t".repeat(tabCount - 1) +
+          code.substring(start);
+        textRef.value = value;
+        setCode(value);
+      } else {
+        //대괄호 있을 때
+        const value =
+          code.substring(0, start) +
+          "\n" +
+          "\t".repeat(tabCount) +
+          code.substring(start);
+        textRef.value = value;
+        setCode(value);
+      }
+      setCursor(start + tabCount + 1);
     }
   };
-  // if(e.key === "Tab") {
-  //   e.preventDefault();
-  //   const start = textRef.selectionStart;
-  //   const end = textRef.selectionEnd;
-  //   const value = code.substring(0, start) + "  " + code.substring(end);
-  //   textRef.value = value;
-  //   setCode(
-  //     code.substring(0, start) + "\t" + code.substring(end, code.length)
-  //   );
-  //   e.target.selectionStart = e.target.selectionEnd = start + 1;
-  // }
 
   return (
     <div>
@@ -180,13 +261,14 @@ const CodeEditor = () => {
           />
           <pre className="code-editor__present">
             <code
+              onInput={handleResizeHeight}
               dangerouslySetInnerHTML={createMarkUpCode(highlightedHTML)}
             ></code>
           </pre>
         </div>
       </div>
       <div>
-        <textarea className="text-viewer" value={copyCode} readOnly></textarea>
+        <textarea className="text-viewer" value={copyCode}></textarea>
       </div>
     </div>
   );
